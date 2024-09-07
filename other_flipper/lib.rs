0e47d5f92078ce72a756f1a63250af9ccc74d5b9
CocDap <hodinhdung1997@gmail.com>
2024-09-07 22:34:22 +0700

    add other flip trait in other flipper contract

--- other_flipper/lib.rs
+++ other_flipper/lib.rs
@@ -6,8 +6,21 @@ pub use self::other_flipper::{
     OtherFlipperRef,
 };
 
+#[ink::trait_definition]
+pub trait OtherFlip {
+    // định nghĩa abstract method flip 
+    #[ink(message)]
+    fn flip(&mut self);
+
+    // định nghĩa abstract method get 
+    #[ink(message)]
+    fn get(&self) -> bool;
+}
+
+
 #[ink::contract]
 mod other_flipper {
+    use super::OtherFlip;
 
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
@@ -32,116 +45,19 @@ mod other_flipper {
         pub fn default() -> Self {
             Self::new(Default::default())
         }
+    }
 
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
+    impl OtherFlip for OtherFlipper {
         #[ink(message)]
-        pub fn flip(&mut self) {
+        fn flip(&mut self) {
             self.value = !self.value;
         }
 
-        /// Simply returns the current value of our `bool`.
         #[ink(message)]
-        pub fn get(&self) -> bool {
+        fn get(&self) -> bool {
             self.value
         }
+
     }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let other_flipper = OtherFlipper::default();
-            assert_eq!(other_flipper.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut other_flipper = OtherFlipper::new(false);
-            assert_eq!(other_flipper.get(), false);
-            other_flipper.flip();
-            assert_eq!(other_flipper.get(), true);
-        }
-    }
-
-
-    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
-    ///
-    /// When running these you need to make sure that you:
-    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-    /// - Are running a Substrate node which contains `pallet-contracts` in the background
-    #[cfg(all(test, feature = "e2e-tests"))]
-    mod e2e_tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// A helper function used for calling contract messages.
-        use ink_e2e::ContractsBackend;
-
-        /// The End-to-End test `Result` type.
-        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-        /// We test that we can upload and instantiate the contract using its default constructor.
-        #[ink_e2e::test]
-        async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let mut constructor = OtherFlipperRef::default();
-
-            // When
-            let contract = client
-                .instantiate("other_flipper", &ink_e2e::alice(), &mut constructor)
-                .submit()
-                .await
-                .expect("instantiate failed");
-            let call_builder = contract.call_builder::<OtherFlipper>();
-
-            // Then
-            let get = call_builder.get();
-            let get_result = client.call(&ink_e2e::alice(), &get).dry_run().await?;
-            assert!(matches!(get_result.return_value(), false));
-
-            Ok(())
-        }
-
-        /// We test that we can read and write a value from the on-chain contract.
-        #[ink_e2e::test]
-        async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let mut constructor = OtherFlipperRef::new(false);
-            let contract = client
-                .instantiate("other_flipper", &ink_e2e::bob(), &mut constructor)
-                .submit()
-                .await
-                .expect("instantiate failed");
-            let mut call_builder = contract.call_builder::<OtherFlipper>();
-
-            let get = call_builder.get();
-            let get_result = client.call(&ink_e2e::bob(), &get).dry_run().await?;
-            assert!(matches!(get_result.return_value(), false));
-
-            // When
-            let flip = call_builder.flip();
-            let _flip_result = client
-                .call(&ink_e2e::bob(), &flip)
-                .submit()
-                .await
-                .expect("flip failed");
-
-            // Then
-            let get = call_builder.get();
-            let get_result = client.call(&ink_e2e::bob(), &get).dry_run().await?;
-            assert!(matches!(get_result.return_value(), true));
-
-            Ok(())
-        }
-    }
 }
