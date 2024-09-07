0330a321e1483d3a8acca065a509219990b1e486
CocDap <hodinhdung1997@gmail.com>
2024-09-07 21:31:11 +0700

    feat: add other flipper and script build

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,3 +3,6 @@
 # Build flipper contract  
 cargo contract build --manifest-path flipper/Cargo.toml
 
+
+# Build other_flipper contract  
+cargo contract build --manifest-path other_flipper/Cargo.toml
